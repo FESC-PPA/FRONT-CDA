0f7fb4ad4381e429ec70fde8a47c93796321358a
Toño <sr.periky@gmail.com>
2023-11-14 17:28:32 -0500

    Se crearon los demás servicios

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -5,8 +5,13 @@ const BASE_URL: string = "https://s4sng6-3000.csb.app";
 export const AUTH_LOGIN_ENDPOINT: string = `${BASE_URL}/auth/login`;
 export const AUTH_REGISTER_ENDPOINT: string = `${BASE_URL}/auth/register`;
 export const BASED_ENDPOINT: string = `${BASE_URL}/based`;
+export const EXCUSE_ENDPOINT: string = `${BASE_URL}/excuse`;
+export const FACIAL_DATA_ENDPOINT: string = `${BASE_URL}/facial-data`;
+export const ORGANIZATION_ENDPOINT: string = `${BASE_URL}/organization`;
+export const SCHEDULE_ENDPOINT: string = `${BASE_URL}/schedule`;
+export const USER_ENDPOINT: string = `${BASE_URL}/user`;
+export const USER_SCHEDULE_ENDPOINT: string = `${BASE_URL}/user-schedule`;
 export const WEEKDAYS_ENDPOINT: string = `${BASE_URL}/week-days`;
 export const WORKDAYS_ENDPOINT: string = `${BASE_URL}/work-days`;
-export const SCHEDULE_ENDPOINT: string = `${BASE_URL}/schedule`;
 
 //export { AUTH_LOGIN_ENDPOINT, AUTH_REGISTER_ENDPOINT, BASED_ENDPOINT, WEEKDAYS_ENDPOINT, WORKDAYS_ENDPOINT, SCHEDULE_ENDPOINT }
--- src/services/app.ts
+++ src/services/app.ts
@@ -1,10 +1,15 @@
-import { BASED_ENDPOINT, SCHEDULE_ENDPOINT, WEEKDAYS_ENDPOINT } from "../constants"
-import { Based, Schedule, Weekdays, Workdays } from "../types"
+import { BASED_ENDPOINT, EXCUSE_ENDPOINT, FACIAL_DATA_ENDPOINT, ORGANIZATION_ENDPOINT, SCHEDULE_ENDPOINT, USER_ENDPOINT, USER_SCHEDULE_ENDPOINT, WEEKDAYS_ENDPOINT } from "../constants"
+import { Based, Excuse, FacialData, Organization, Schedule, User, UserSchedule, Weekdays, Workdays } from "../types"
 import { useService } from "."
 
 export const useBasedService = () => useService<Based>(BASED_ENDPOINT)
+export const useExcuseService = () => useService<Excuse>(EXCUSE_ENDPOINT)
+export const useFacialDataService = () => useService<FacialData>(FACIAL_DATA_ENDPOINT)
+export const useScheduleService = () => useService<Schedule>(SCHEDULE_ENDPOINT)
+export const userOrganizationService = () => useService<Organization>(ORGANIZATION_ENDPOINT)
+export const useUserService = () => useService<User>(USER_ENDPOINT)
+export const useUserScheduleService = () => useService<UserSchedule>(USER_SCHEDULE_ENDPOINT)
 export const useWeekDaysService = () => useService<Weekdays>(WEEKDAYS_ENDPOINT)
 export const useWorkDaysService = () => useService<Workdays>(WEEKDAYS_ENDPOINT)
-export const useScheduleService = () => useService<Schedule>(SCHEDULE_ENDPOINT)
 
 //export { useBasedService, useWeekDaysService, useWorkDaysService, useScheduleService }
